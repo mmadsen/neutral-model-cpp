fac143652d5ff7240f4d8ba81c7c48c8a6b71063
Mark Madsen <mark@madsenlab.org>
2015-08-29 12:48:03 -0700

    Removing a script which just ran the program in a loop, from a memory debugging session

--- stress.sh
+++
@@ -1,6 +0,0 @@
-export OMP_NUM_THREADS=4
-for d in `seq 10`;
-do 
- /usr/bin/time ./neutral-test --popsize 100000 --numloci 5 --inittraits 6 --innovrate 0.0001 --simlength 25000 --debug 1 --ruletype wfia
- sleep 1
-done
